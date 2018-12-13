7553fe197ac757356c7a041ded5ada1589036791
Daniel Parks <dp+git@oxidized.org>
2018-12-13 01:44:20 -0800

    Simplify code for cygwin_home fact

--- lib/facter/cygwin_home.rb
+++ lib/facter/cygwin_home.rb
@@ -1,14 +1,7 @@
 Facter.add(:cygwin_home) do
   confine :osfamily => :windows
 
-  PATHS = ['C:\Cygwin64', 'C:\Cygwin']
   setcode do
-    path = nil
-    PATHS.each do |p|
-      path = p if File.exist? p
-      break
-    end
-
-    path
+    ['C:\Cygwin64', 'C:\Cygwin'].find {|path| File.exist? path}
   end
 end
